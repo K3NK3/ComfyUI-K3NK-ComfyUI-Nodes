832f98f2fa6ace4093bde10217d11863b17c5390
K3NK <32736457+K3NK3@users.noreply.github.com>
2026-05-24 11:01:43 +0200

    Update __nodes__.py

--- __nodes__.py
+++ __nodes__.py
@@ -3,7 +3,9 @@ NODE_CLASS_MAPPINGS = {
     "K3NKImageLoaderWithBlending": "K3NKImageLoaderWithBlending",
     "K3NKSaveLatentPassThrought": "K3NKSaveLatentPassThrought",
     "K3NKFindNearestBucket": "K3NKFindNearestBucket",
+    "K3NKImageOverlay": "K3NKImageOverlay",
     "K3NKVideoLoaderFromDir": "K3NKVideoLoaderFromDir",
     "K3NKTextLoaderFromDir": "K3NKTextLoaderFromDir",
+    "K3NKVideoConcatSimple": "K3NKVideoConcatSimple",
     "K3NKSaveCompress": "K3NKSaveCompress",
 }
